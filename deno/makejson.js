3cbd93318ef92df162f1833da51dc03b5d77bc5f
Taisuke Fukuno <fukuno@jig.jp>
2021-04-19 17:57:19 +0900

    fix path

--- deno/makejson.js
+++ deno/makejson.js
@@ -1,5 +1,6 @@
 //import { dir2json } from "../../../code4sabe-js/dir2json.js";
-import { dir2json } from "./dir2json.js";
+//import { dir2json } from "./dir2json.js";
+import { dir2json } from "https://js.sabae.cc/dir2json.js";
 
 const d = await dir2json("../res/ぴぽや32×32キャラチップ素材(キャラクターなんとかJ用)");
 await Deno.writeTextFile("../res/res.json", JSON.stringify(d, null, 2));
