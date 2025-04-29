f9882f0f5f9056ce5ab7b534d9645ca8d961bd11
Taisuke Fukuno <fukuno@jig.jp>
2025-04-29 18:19:35 +0900

    renamed images

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # egcharmaker
-http://codeforkosen.github.io/egcharmaker/  
-* 日本語ファイル名の関係で調整中  
+
+- http://codeforkosen.github.io/egcharmaker/
 
 ## キャラクター
-ぴぽや倉庫  
-https://pipoya.net/sozai/  
 
-キャラクターチップ２ - ぴぽや倉庫  
-https://pipoya.net/sozai/assets/charachip/character-chip-2/  
+- [ぴぽや倉庫](https://pipoya.net/sozai/)
+- [キャラクターチップ２ - ぴぽや倉庫](https://pipoya.net/sozai/assets/charachip/character-chip-2/)
--- deno/makejson.js
+++ deno/makejson.js
@@ -1,6 +1,31 @@
 import { dir2json } from "./dir2json.js";
 //import { dir2json } from "https://js.sabae.cc/dir2json.js";
 
-const d = await dir2json("../res/ぴぽや32×32キャラチップ素材(キャラクターなんとかJ用)");
-await Deno.writeTextFile("../res/res.json", JSON.stringify(d, null, 2));
+const path = "../res/ぴぽや32×32キャラチップ素材(キャラクターなんとかJ用)";
+const d = await dir2json(path);
+
+await Deno.mkdir("../data", { recursive: true });
+
+let idx = 1;
+const writeData = async (path, d) => {
+  for (const name in d) {
+    const o = d[name];
+    if (o) {
+      await writeData(path + "/" + name, o);
+    } else {
+      if (!name.endsWith(".png")) {
+        delete d[name];
+      } else {
+        const fn = idx++ + ".png";
+        d[name] = fn;
+        const bin = await Deno.readFile(path + "/" + name);
+        await Deno.writeFile("../data/" + fn, bin);
+        console.log(fn + " <- " + name);
+      }
+    }
+  }
+};
+await writeData(path, d);
+
+await Deno.writeTextFile("../data.json", JSON.stringify(d, null, 2));
 console.log(d);
